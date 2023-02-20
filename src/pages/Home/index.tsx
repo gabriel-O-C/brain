7043206bf175d98678f6a809775da06edcaa8841
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-20 16:56:25 -0300

    feat: add main content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,23 @@ import { Header } from "../../components";
 export const Home: React.FC = () => {
   return (
     <>
-    <Header />
+      <Header />
+      <main className="flex flex-1 flex-col items-center">
+        <h1>What this site is about?</h1>
+        <p>
+          I've made this site to center what I learn in a single place. This
+          whole idea came from the <strong>learn in public</strong> method, and
+          my will to try new technologies and improve my understandment of
+          another ones.
+        </p>
+      </main>
+      <section>
+        <h2>Who am I?</h2>
+        {/*
+         TODO: write a bio
+         */}
+        <p>My name is Gabriel Chaves, I'm a software developer. </p>
+      </section>
     </>
   );
 };
