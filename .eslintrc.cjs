4a265a80f5e2ffcc8e6a6c331d77ba16a4560e0f
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-20 14:49:04 -0300

    chore: add eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,17 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript'
+    'standard-with-typescript',
+    'prettier'
   ],
   overrides: [
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: "./tsconfig.json"
   },
+
   plugins: [
     'react'
   ],
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  trailingComma: "all",
+  bracketSameLine: true,
+  arrowParens: "avoid",
+};
