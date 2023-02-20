c2602e188f67680a9a40a9fe030f03bd098ee823
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-02-20 16:38:31 -0300

    refactor: move Header to its own file

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Menu } from "../Menu";
+import menu from "../../assets/menu.svg";
+
+export const Header: React.FC = () => {
+  const [isOpen, setIsopen] = useState(false);
+
+  return (
+    <header>
+    <div className="flex justify-between p-4 w-screen">
+      <strong className="uppercase">MyNotes</strong>
+      <nav className="cursor-pointer relative">
+        <img
+          alt="menu-icon"
+          role="menubar"
+          src={menu}
+          title="menu"
+          onClick={() => {
+            setIsopen(prevState => !prevState);
+          }}
+        />
+        {isOpen ? <Menu /> : null}
+      </nav>
+    </div>
+  </header>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
-import menu from "../../assets/menu.svg";
-import { Menu } from "../../components/menu";
+/* eslint-disable react/no-unescaped-entities */
+import { Header } from "../../components";
 
 export const Home: React.FC = () => {
-  const [isOpen, setIsopen] = useState(false);
   return (
-    <header>
-      <div className="flex justify-between p-4 w-screen">
-        <strong className="uppercase">MyNotes</strong>
-        <nav className="cursor-pointer relative">
-          <img
-            alt="menu-icon"
-            role="menubar"
-            src={menu}
-            title="menu"
-            onClick={() => {
-              setIsopen(prevState => !prevState);
-            }}
-          />
-          {isOpen ? <Menu /> : null}
-        </nav>
-      </div>
-    </header>
+    <>
+    <Header />
+    </>
   );
 };
